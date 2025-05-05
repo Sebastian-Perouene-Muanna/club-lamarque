6ba5e9da1dae38ffe28d06e5e8b07cf348b011c0
Sebastian Perouene Muanna <sebastianperouene@gmail.com>
2025-05-05 12:47:05 -0300

    Descripción de los cambios realizados

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/club-lamarque/", // Este valor debe coincidir con el nombre del repositorio en GitHub
+  base: "/club-lamarque/",  // Esto debe coincidir con el nombre del repositorio
   plugins: [react()],
 });
